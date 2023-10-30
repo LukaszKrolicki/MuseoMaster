477afb66f0a15ed574fda478155d43098a804011
SesneiBunny <lukasmatrix12345@gmail.com>
2023-10-30 12:20:47 +0100

    test1

--- src/main/java/com/museomaster/museomaster/App.java
+++ src/main/java/com/museomaster/museomaster/App.java
@@ -1,4 +1,15 @@
 package com.museomaster.museomaster;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
