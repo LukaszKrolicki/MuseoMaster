c91c898063c2821057ace9d9010fc8ad84c70692
Jakub <jakub.kucaba@gmail.com>
2023-12-11 09:51:00 +0100

    Confirm Button added in Konserwator Exh List

--- src/main/java/com/museomaster/museomaster/TypyUzytkownikow/ZwyklyKonswerwator/ListaPrzydzielonychZaabytkowController.java
+++ src/main/java/com/museomaster/museomaster/TypyUzytkownikow/ZwyklyKonswerwator/ListaPrzydzielonychZaabytkowController.java
@@ -1,6 +1,7 @@
 package com.museomaster.museomaster.TypyUzytkownikow.ZwyklyKonswerwator;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
@@ -8,6 +9,7 @@ import java.util.ResourceBundle;
 
 public class ListaPrzydzielonychZaabytkowController implements Initializable {
     public ListView lista_przydziel_zab;
+    public Button zatwierdz_btn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
