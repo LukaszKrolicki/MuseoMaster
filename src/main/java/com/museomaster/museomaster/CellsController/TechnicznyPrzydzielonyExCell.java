e17b04fa915fb5dd6033e3ba7004ac6bc7616b30
SesneiBunny <lukasmatrix12345@gmail.com>
2023-12-11 09:55:50 +0100

    Technician cell added

---
+++ src/main/java/com/museomaster/museomaster/CellsController/TechnicznyPrzydzielonyExCell.java
@@ -0,0 +1,21 @@
+package com.museomaster.museomaster.CellsController;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TechnicznyPrzydzielonyExCell implements Initializable {
+    public CheckBox check_przzeniesione;
+    public Label id_zad;
+    public Label nazwa_zabytku;
+    public Label skad_przeniesc_zabytek;
+    public Label dokad_przeniesc_zabytek;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+}
